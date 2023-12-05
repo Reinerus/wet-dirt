6d3444054456bdba11cbdcf537d412703cd211de
JoshKlein16 <joshuak4890@gmail.com>
2023-12-05 11:08:43 -0500

    functional login system

--- login.js
+++ login.js
@@ -4,9 +4,9 @@ function Signup() {
     var password = document.getElementById("password").value;
 
     var user = {
-        username:username,
-        email:email,
-        password:password,
+        username: username,
+        email: email,
+        password: password,
     };
 
     localStorage.setItem(email, JSON.stringify(user));
@@ -17,20 +17,19 @@ function Signup() {
 function Login(){
     var emailLogin = document.getElementById("lemail").value;
     var password = document.getElementById("lpassword").value;
-    console.log(emailLogin)
+    var storedUserData = localStorage.getItem(emailLogin);
 
-    if(emailLogin = localStorage.getItem('email')){
-        user = JSON.parse(localStorage.getItem(email));
-        console.log(username)
-        if(password===localStorage.getItem(email)){
+    if(storedUserData) {
+        var storedUser = JSON.parse(storedUserData)
+        if(password === storedUser.password){
             location.replace("home.html")
         } 
         else {
-            alert("Incorrect Password")
+            alert("Incorrect Password");
         }
     }
     else {
-        alert("email does not exist")
+        alert("email does not exist");
     }
 
 }
