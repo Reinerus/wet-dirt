f0e996738eb6b3c44076abb58065fe6098dc64b2
Peyton Meyer <peytonmeyer23@gmail.com>
2023-12-12 09:55:34 -0500

    merch

--- merch.js
+++ merch.js
@@ -5,70 +5,49 @@ let listCard = document.querySelector('.listCard');
 let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
-
 openShopping.addEventListener('click', ()=>{
     body.classList.add('active');
 })
-
 closeShopping.addEventListener('click', ()=>{
     body.classList.remove('active');
 })
 
-
-
-let listCards = [];
-function initApp(){
-    mproducts.forEach((value, key)=>{
-
 let products = [
     {
         id: 1,
-        name: 'Mudzzarella Sticks',
-        desc: 'Deep fried and delicious sticks of local mud created in house.',
-        image: 'images/mozzarrekea.png',
-        price: 12.99
+        name: 'Müddy Shirt',
+        desc: 'Rep the Müd everywhere you go!.',
+        image: 'images/merch/mudddddd - Copy.png',
+        price: 119.99
     },
     {
         id: 2,
-        name: 'Bloomin Mudions',
-        desc: 'Fried, battered, and finely harvested mud from our own backyard.',
-        image: 'images/blloming_mduios.png',
-        price: 14.99
+        name: 'Müddie',
+        desc: 'Hoodie + Müd! What else is gonna keep you warm..',
+        image: 'images/merch/hoodie.png',
+        price: 149.99
     },
     {
         id: 3,
-        name: 'Mud Stuffed Mud',
-        desc: 'Finely pressed mud wrapped in the highest quality local mud in the country.',
-        image: 'images/mudzoneeee.png',
-        price: 9.99
+        name: 'Müd hat',
+        desc: 'rep the Müd on top of your head.',
+        image: 'images/merch/hat shirt.png',
+        price: 99.99
     },
     {
         id: 4,
-        name: 'Buffalo Mud Dip',
-        desc: 'Shredded mud mixed with buffalo sauce and locally bought  ese.',
-        image: 'images/buffalo_mud_dip.png',
-        price: 19.99
-    },
-    {
-        id: 5,
-        name: 'Mud Balls',
-        desc: 'Perfectly seasoned and spiced mud balls served with a side of mudinara sauce',
-        image: 'images/mudballs.png',
-        price: 14.99
-    },
-    {
-        id: 6,
-        name: 'Mudsanga',
-        desc: 'Stacked layers of pasta packed with meat and cheese, covered in mud sauce.   ',
-        image: 'images/mudsagna.png',
-        price: 26.99
+        name: 'Wet-Dirt shirt',
+        desc: 'The wettest and dirtiest shirt you will evert own.',
+        image: 'images/merch/shirt temp.png',
+        price: 119.99
     },
 ];
+
+
 
 let listCards = [];
 function initApp(){
     products.forEach((value, key)=>{
-
         let newDiv = document.createElement('div');
         newDiv.classList.add('item');
         newDiv.innerHTML = `
@@ -83,15 +62,25 @@ function initApp(){
 }
 initApp();
 
+function loadCartFromStorage(){
+    listCards = JSON.parse(localStorage.getItem('cartItems')) || [];
+    reloadCard()
+}
+
+loadCartFromStorage();
+
+function saveCartToStorage(){
+    localStorage.setItem('cartItems', JSON.stringify(listCards));
+}
+
 function addToCard(key){
     if(listCards[key] == null){
-
-        listCards[key] = mproducts[key];
+        listCards[key] = products[key];
         listCards[key].quantity = 1;
-        prices = mproducts[key].price
-
+        prices = products[key].price
     }
     reloadCard()
+    saveCartToStorage();
 }
 
 function reloadCard(){
@@ -121,6 +110,7 @@ function reloadCard(){
     })
     total.innerText = totalPrice.toLocaleString();
     quantity.innerText = count;
+    saveCartToStorage()
 }
 
 function changeQuantity(key, quantity){
@@ -130,6 +120,7 @@ function changeQuantity(key, quantity){
         listCards[key].quantity = quantity;
         listCards[key].price = quantity * prices;
     }
+    saveCartToStorage()
     reloadCard();
 }
 localStorage.setItem('listCards[key]', JSON.stringify(listCards[key]));
@@ -140,3 +131,27 @@ myButton.addEventListener("click", function() {
   var inputValue = myInput.value;
   console.log(inputValue);
 });
+
+function checkOut(){
+    location.replace("payment.html")
+}
+
+function storeproductdata() {
+    var id = document.getElementById("id").value;
+    var desc = document.getElementById("desc").value;
+    var price = document.getElementById("price").value;
+    var data = { id: id, price: price, desc: desc };
+
+    var newProduct = {
+        
+        id:id,
+        price:price,
+        desc:desc,
+    }
+
+    var storedData = JSON.parse(localStorage.getItem("products")) || [];
+    storedData.push(newProduct);
+    console.log(id)
+    localStorage.setItem("products", JSON.stringify(storedData));
+  }
+  var storedData = JSON.parse(localStorage.getItem("products")) || [];
